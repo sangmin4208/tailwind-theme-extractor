13fadb96a7089cb3e1ce2ee2f6c5ee99a33c34d6
Lee SangMin <sangmin4208@gmail.com>
2025-02-01 03:29:12 +0900

    :art: extract theme 페이지 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,55 @@
+'use client'
+
+import { useRef, useState } from 'react'
+import { Textarea } from '@/components/ui/textarea'
+import { extractTailwindTheme } from '@/lib/utils/extract-tailwind-theme'
+import { useCopyToClipboard } from 'usehooks-ts'
 import { Button } from '@/components/ui/button'
+import { Copy } from 'lucide-react'
+import { toast } from 'sonner'
 
 export default function Home() {
+  const [tailwindTheme, setTailwindTheme] = useState(extractTailwindTheme(''))
+  const [_, copy] = useCopyToClipboard()
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
+
+  const handleChange = (e) => {
+    const result = extractTailwindTheme(e.target.value)
+    setTailwindTheme(result)
+  }
+  const handleInput = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto' // 높이 초기화
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px` // 내용에 맞게 높이 조절
+    }
+  }
+
+  const handleCopy = async () => {
+    await copy(JSON.stringify(tailwindTheme, null, 2))
+    toast.success('Copied to clipboard!')
+  }
   return (
     <div className={'container'}>
-      <h1>Home</h1>
-      <p>Welcome to the home page.</p>
-      <Button>Hello</Button>
+      <h1 className={'my-2 text-2xl font-bold'}>Extract Tailwind Theme</h1>
+
+      <section className={'mt-10 grid grid-cols-2 items-start gap-4'}>
+        <Textarea
+          ref={textareaRef}
+          placeholder="Type your Tailwind CSS here"
+          onChange={handleChange}
+          onInput={handleInput} // 입력될 때마다 크기 조정
+          className={'min-h-[600px] resize-none overflow-hidden'} // 기본 크기 조정 비활성화
+        />
+
+        <div
+          className={'bg-primary relative text-primary-foreground rounded p-5 text-sm'}
+        >
+          <Button onClick={handleCopy} className={'absolute top-5 right-5'}>
+            <Copy/>
+          </Button>
+          <pre>{JSON.stringify(tailwindTheme, null, 2)}</pre>
+        </div>
+      </section>
     </div>
   )
 }
--- src/lib/utils/extract-tailwind-theme.ts
+++ src/lib/utils/extract-tailwind-theme.ts
@@ -1,20 +1,37 @@
-
-type ThemeCategory = 'color' | 'text' | 'shadow' | 'radius' | 'font' | 'spacing' | 'breakpoint' | 'container' | 'tracking' | 'leading' | 'blur' | 'animation';
+type ThemeCategory =
+  | 'color'
+  | 'text'
+  | 'shadow'
+  | 'radius'
+  | 'font'
+  | 'spacing'
+  | 'breakpoint'
+  | 'container'
+  | 'tracking'
+  | 'leading'
+  | 'blur'
+  | 'animation'
 
 type ThemeVariable = {
-  name: string;
-  value: string;
-};
+  name: string
+  value: string
+}
 
 type TailwindThemeType = {
-  [key in ThemeCategory]: ThemeVariable[];
-};
+  [key in ThemeCategory]: ThemeVariable[]
+}
 
-const parseVariables = (css: string, prefix: ThemeCategory): ThemeVariable[] => {
-  const regex = new RegExp(`--${prefix}-([a-zA-Z0-9-]+):\s*([^;]+);`, 'g');
-  const matches = [...css.matchAll(regex)].map(match => ({ name: match[1], value: match[2].trim() }));
-  return matches.filter(({ name }) => name !== '*');
-};
+const parseVariables = (
+  css: string,
+  prefix: ThemeCategory
+): ThemeVariable[] => {
+  const regex = new RegExp(`--${prefix}-([a-zA-Z0-9-]+):\s*([^;]+);`, 'g')
+  const matches = [...css.matchAll(regex)].map((match) => ({
+    name: match[1],
+    value: match[2].trim(),
+  }))
+  return matches.filter(({ name }) => name !== '*')
+}
 
 export const extractTailwindTheme = (cssString: string): TailwindThemeType => {
   return {
@@ -30,5 +47,5 @@ export const extractTailwindTheme = (cssString: string): TailwindThemeType => {
     leading: parseVariables(cssString, 'leading'),
     blur: parseVariables(cssString, 'blur'),
     animation: parseVariables(cssString, 'animation'),
-  };
-};
+  }
+}
