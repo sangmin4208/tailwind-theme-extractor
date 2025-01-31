5f4a5d00d773f91925f70db61044a8ed4fbf5de1
Lee SangMin <sangmin4208@gmail.com>
2025-02-01 00:54:51 +0900

    :package: setup shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
-    <div>
+    <div className={'container'}>
       <h1>Home</h1>
       <p>Welcome to the home page.</p>
+      <Button>Hello</Button>
     </div>
   )
 }
